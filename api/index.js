172c5206d8df9d3b6002b731104c82f037c4c8ea
Maxim <334890@gmail.com>
2023-11-17 21:22:33 +0600

    try cjs

--- api/index.js
+++ api/index.js
@@ -1,16 +1,19 @@
-import dotenv from "dotenv";
+/* import dotenv from "dotenv";
 import express, { json } from "express";
 import cors from "cors";
 import distributorsRouter from "./routes/distributors.js";
 import productsRouter from "./routes/products.js";
 import mongoose from "mongoose";
-import { Product } from "./model/product.js";
+import { Product } from "./model/product.js"; */
 //const PORT = process.env.PORT || 3000;
-dotenv.config();
+const express = require("express");
+const cors = require("cors");
+
+//dotenv.config();
 
 const app = express();
 app.use(cors({ credentials: true }));
-app.use(json());
+app.use(express.json());
 
 //app.use("/api/distributors", distributorsRouter);
 //app.use("/api/products", productsRouter);
